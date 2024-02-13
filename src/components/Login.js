9fd507f2567a8a4a31ff48e2beec6b8f89ca6d41
linovorefresh <linolp@gmail.com>
2024-02-13 15:55:24 -0700

    login state, onchange and submit func

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
 import './login.css';
 import background from '../images/hero.jpg' 
 
 const Login = () => {
+
+    const [values, setValues] = useState({
+        email: '',
+        password: ''
+    });
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        
+        axios.post('http://localhost:3002/auth/adminlogin')
+            .then(result => console.log(result))
+            .catch(err => console.log(err))
+    }
+
   return (
     <div className='d-flex justify-content-center align-items-center vh-100 loginPage' style={{backgroundImage: `url(${background})`}}>
       <div className='p-3 rounded w-25 border loginForm'>
         <h2> Login </h2>
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className='mb-2'>
                 <label htmlFor='email'> <strong>Email:</strong></label>
-                <input type='email' name='email' autoComplete='off' placeholder='Enter email' className='form-control rounded-0'></input>
+                <input 
+                    type='email' 
+                    name='email' 
+                    autoComplete='off' 
+                    placeholder='Enter email' 
+                    className='form-control rounded-0'
+                    onChange={(e) => setValues({...values, email: e.target.value})}
+                />
             </div>
             <div className='mb-3'>
                 <label htmlFor='password'> <strong>Password:</strong></label>
-                <input type='password' name='password' autoComplete='off' placeholder='Enter password' className='form-control rounded-0'></input>
+                <input 
+                    type='password' 
+                    name='password' 
+                    autoComplete='off' 
+                    placeholder='Enter password' 
+                    className='form-control rounded-0'
+                    onChange={(e) => setValues({...values, email: e.target.value})}
+                />
             </div>
             <button className='btn btn-primary w-100 rounded-0 mb-2'>Log in</button>
             <div className='mb-2'>
